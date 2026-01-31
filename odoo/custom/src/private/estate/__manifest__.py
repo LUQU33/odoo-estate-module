e3c72ddd9eafeffbaf2170ae90b6529e7755b67f
Pedro Luque <pedroluque481@gmail.com>
2026-01-31 18:20:07 -0300

    Estructura base Odoo + modulo real estate

---
+++ odoo/custom/src/private/estate/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Real Estate",
+    "depends": ["base"],
+    "author": "Pedro Luque",
+    "summary": "SI LEYERAS ESTO SABRIAS QUE SOY PEDRO",
+    "application": True,
+    "license": "AGPL-3",
+}
