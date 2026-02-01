6848bfee5a6ca20c6d83b54795511e77fbd7ac68
Pedro Luque <pedroluque481@gmail.com>
2026-02-01 17:46:40 -0300

    feat(views): definir action y menus para estate.property

--- odoo/custom/src/private/estate/__manifest__.py
+++ odoo/custom/src/private/estate/__manifest__.py
@@ -3,7 +3,10 @@
     "depends": ["base"],
     "author": "LUQU3",
     "summary": "SI LEYERAS ESTO SABRIAS QUE SOY PEDRO",
-    "data": ["security/ir.model.access.csv"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+    ],
     "application": True,
     "license": "AGPL-3",
 }
