eb6f21d501937a7ed10178dfe88d7112889b5e5e
Pedro Luque <pedroluque481@gmail.com>
2026-02-01 17:09:49 -0300

    Feat: Reglas de acceso (ACL) para estate.property

--- odoo/custom/src/private/estate/__manifest__.py
+++ odoo/custom/src/private/estate/__manifest__.py
@@ -3,6 +3,7 @@
     "depends": ["base"],
     "author": "LUQU3",
     "summary": "SI LEYERAS ESTO SABRIAS QUE SOY PEDRO",
+    "data": ["security/ir.model.access.csv"],
     "application": True,
     "license": "AGPL-3",
 }
