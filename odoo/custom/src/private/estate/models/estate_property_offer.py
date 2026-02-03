5bcbc4c28cc964943c3f57d662a767ac0dad4e60
Pedro Luque <pedroluque481@gmail.com>
2026-02-03 11:05:06 -0300

    feat(models/views): agregar dias y fecha de caducidad en ofertas (computed inverse fields)

--- odoo/custom/src/private/estate/models/estate_property_offer.py
+++ odoo/custom/src/private/estate/models/estate_property_offer.py
@@ -1,4 +1,6 @@
-from odoo import _, fields, models
+from datetime import timedelta
+
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
@@ -14,6 +16,31 @@ class EstatePropertyOffer(models.Model):
 
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+
+    validity = fields.Integer(default=7, string="Validity (days)")
+
+    date_deadline = fields.Date(
+        string="Deadline",
+        compute="_compute_date_deadline",
+        inverse="_inverse_date_deadline",
+    )
+
+    @api.depends("validity", "create_date")
+    def _compute_date_deadline(self):
+        for record in self:
+            date = (
+                record.create_date.date() if record.create_date else fields.Date.today()
+            )
+
+            record.date_deadline = date + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            date = (
+                record.create_date.date() if record.create_date else fields.Date.today()
+            )
+            if record.date_deadline:
+                record.validity = (record.date_deadline - date).days
 
     def action_accept(self):
         for record in self:
